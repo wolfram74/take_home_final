808b77de9dd976fb3000924c75a43e21a686f1ad
wolfram74 <phaugen@gmail.com>
2017-05-11 16:44:40 -0500

    first pass on decomposition finished, moving on to evolution

---
+++ source/analytic_time_evolution.py
@@ -0,0 +1,29 @@
+import numpy
+import tools
+from matplotlib import pyplot
+
+params = {
+    'kappa': 1.0,
+    'steps': 100000,
+    'foldings': 5.0,
+    'modes':100
+}
+
+def main():
+    del_t = (params['foldings'])/(params['kappa']*params['steps']*numpy.pi**2)
+    print('del_t %f' % del_t )
+    x_vals, temperature_vals = tools.data_loader()
+    temperature_left = temperature_vals[0]
+    stable_gradient = (temperature_vals[-1]-temperature_vals[0])/x_vals[-1]
+    stable_temperature = temperature_left + stable_gradient*x_vals
+
+    transient_temperatures = temperature_vals-stable_temperature
+    fourier_coefficients = []
+    indices = numpy.arange(1, params['modes']+1)
+    for i in range(1,params['modes']+1):
+        fourier_coefficients.append(tools.fourier_coefficient(i, transient_temperatures, x_vals))
+    print(len(indices), len(fourier_coefficients))
+    return
+
+if __name__ == "__main__":
+    main()
--- source/decomposition.py
+++ source/decomposition.py
@@ -11,21 +11,20 @@ def main():
     print(stable_temperature[0], stable_temperature[-1])
     print(x_vals[0], x_vals[-1])
     transient_temperatures = temperature_vals-stable_temperature
-    # pyplot.plot(x_vals, temperature_vals)
-    # pyplot.plot(x_vals, transient_temperatures)
-    # pyplot.show()
     fourier_coefficients = []
-    for i in range(1,100):
+    normalized_error =[]
+    for i in range(1,101):
         fourier_coefficients.append(tools.fourier_coefficient(i, transient_temperatures, x_vals))
     model = tools.recompose(fourier_coefficients, x_vals)
-    # print(i)
-    print(tools.error_measure(fourier_coefficients, transient_temperatures,x_vals))
-    print(tools.error_measure(fourier_coefficients, transient_temperatures,x_vals)/len(x_vals))
+    residues = transient_temperatures-model
     pyplot.plot(x_vals, transient_temperatures)
     pyplot.plot(x_vals, model)
+    pyplot.plot(x_vals, numpy.absolute(residues))
     pyplot.show()
-
-
+    # pyplot.plot(range(1,101), fourier_coefficients)
+    # pyplot.show()
+    # pyplot.plot(range(1,101), numpy.absolute(fourier_coefficients))
+    # pyplot.show()
     return
 
 if __name__ == "__main__":
