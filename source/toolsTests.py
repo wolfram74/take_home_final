d5db42a81cddda968efb19b7f7e03fa55e4cf3de
wolfram74 <phaugen@gmail.com>
2017-05-11 09:35:43 -0500

    testing scaffold set up

---
+++ source/toolsTests.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestTools(unittest.TestCase):
+    def test_fourier_coefficients(self):
+        self.assertEqual(0,0)
+        return
+
+    def test_du_fort_frankel(self):
+        self.assertEqual(0,0)
+        return
