d09f2ff4e9b660024f2476399c8e36a0964e6e24
wolfram74 <phaugen@gmail.com>
2017-05-11 10:14:17 -0500

    basic test cases set up

---
+++ source/tools.py
@@ -0,0 +1,7 @@
+import numpy
+
+def fourier_coefficient(k_val, y_vals):
+    return 0
+
+def du_fort_frankel(y_vals, previous_y_vals, kappa, del_t, del_x):
+    return y_vals
--- source/toolsTests.py
+++ source/toolsTests.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy
 import tools
+import copy
 
 pi = numpy.pi
 
@@ -18,5 +19,19 @@ class TestTools(unittest.TestCase):
         return
 
     def test_du_fort_frankel(self):
-        self.assertEqual(0,0)
+        x_vals = numpy.linspace(0,1, 7)
+        y_vals = numpy.array([2.0, 4.0, 3.5, 2.1, 9.1, 5.3, 4.0])
+        previous_y_vals = copy.copy(y_vals)
+        del_t = 0.01
+        del_x = 1.0/7.0
+        steps = 1000
+        kappa = 0.1
+        for step in range(steps):
+            next_y_vals = tools.du_fort_frankel(
+                y_vals, previous_y_vals,
+                kappa, del_t, del_x
+                )
+            previous_y_vals = y_vals
+            y_vals = next_y_vals
+        self.assertLess(abs(next_y_vals[3]-3.0),10**-6)
         return
